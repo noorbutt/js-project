0c0d3a7ace4915b15f13d564ca29d37b8a88434e
noorbutt <noorbuttnoorbutt706@gmail.com>
2021-07-30 21:14:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log('noor')
+
+
+function blackjackHit() {
+    let cardImage = document.createElement('img');
+    cardImage.src = 'static/images/Q.png' 
+    document.querySelector(YOU['div']).appendChild(cardImage)
+    
+  }
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
